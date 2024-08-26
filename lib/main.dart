43204fde90277b9d42cf8ecb1e0f63d5ed2e5940
anayaher <aheranay@gmail.com>
2024-08-27 01:32:30 +0530

    Added Splash Screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:news/screens/splash_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,12 +12,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      home: const SplashScreen(),
       title: 'Flutter Demo',
       theme: ThemeData(
        
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
+    
      
     );
   }
---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,58 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:news/screens/home_screen.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    Timer(const Duration(seconds: 4), () {
+      Navigator.pushReplacement(
+          context, MaterialPageRoute(builder: (context) => HomeScreen()));
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final height = MediaQuery.sizeOf(context).height * 1;
+
+    return Scaffold(
+      body: Container(
+        padding: const EdgeInsets.all(8),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              'assets/images/splash_pic.jpg',
+              fit: BoxFit.cover,
+              height: height * .5,
+            ),
+            SizedBox(
+              height: height * 0.04,
+            ),
+            Text(
+              'Flutter News',
+              style: GoogleFonts.bebasNeue(
+                  fontSize: 24, letterSpacing: .6, color: Colors.black),
+            ),
+            SizedBox(
+              height: height * 0.04,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
